4c7bcdf451fa0dcc13a58f32bf09eb680b006e8f
Sanegv <sanegv5@gmail.com>
2025-08-19 14:38:37 +0200

    fixed values method signatures

--- interfaces/values/ValueInterface.h
+++ interfaces/values/ValueInterface.h
@@ -5,14 +5,14 @@ namespace Language {
     namespace Values {
         class ValueI {
         public:
-            virtual ValueI* add(const ValueI*)       const = 0;
-            virtual ValueI* operator+(const ValueI*) const = 0;
-            virtual ValueI* sub(const ValueI*)       const = 0;
-            virtual ValueI* operator-(const ValueI*) const = 0;
-            virtual ValueI* mul(const ValueI*)       const = 0;
-            virtual ValueI* operator*(const ValueI*) const = 0;
-            virtual ValueI* div(const ValueI*)       const = 0;
-            virtual ValueI* operator/(const ValueI*) const = 0;
+            virtual ValueI* add(const ValueI* const)       const = 0;
+            virtual ValueI* operator+(const ValueI* const) const = 0;
+            virtual ValueI* sub(const ValueI* const)       const = 0;
+            virtual ValueI* operator-(const ValueI* const) const = 0;
+            virtual ValueI* mul(const ValueI* const)       const = 0;
+            virtual ValueI* operator*(const ValueI* const) const = 0;
+            virtual ValueI* div(const ValueI* const)       const = 0;
+            virtual ValueI* operator/(const ValueI* const) const = 0;
         };
     }
 }
