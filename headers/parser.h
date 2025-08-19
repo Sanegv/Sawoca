a0cf572353123a231102f24085ca1ecffc9e3686
Sanegv <sanegv5@gmail.com>
2025-08-19 17:18:17 +0200

    removed tokens from parser

--- headers/parser.h
+++ headers/parser.h
@@ -1,5 +1,5 @@
-#ifndef DEF_PARSER
-#define DEF_PARSER
+#ifndef DEFINE_PARSER
+#define DEFINE_PARSER
 
 /**
  *@file parser.h
@@ -13,115 +13,6 @@
 #include "lexer.h"
 
 namespace Calc {
-
-	/**
-	 * @brief Represents a parser token, with a type and a value. It's a 
-	 * composite pattern that can either be a literal value, a variable or an 
-	 * operation.
-	 */
-	class Token {
-	private:
-		Token_Type type;
-		Token() = delete;
-	public:
-		/**
-		 * @brief Creates a new token of the specified type. Can only be used 
-		 * in subclasses that inherit Token.
-		 *
-		 * @param the type of the token.
-		 */
-		Token(Token_Type type);
-		virtual ~Token() = default;
-
-		/**
-		 * @brief Accessor for the type field of a token.
-		 *
-		 * @return the type of the token.
-		 */
-		Token_Type get_type();
-
-		/**
-		 * Evaluates the value of a token, whether by simply returning it, or 
-		 * by recursively evaluating the value of its branches in the AST.
-		 *
-		 * @return the evaluated value of the token and its branches in the AST.
-		 */
-		virtual double get_value() = 0;
-	};
-
-	/**
-	 * @brief Token class of literal numeric values.
-	 */
-	class Number_Token : public Token {
-	private:
-		double value;
-	public:
-		/**
-		 * @brief Creates a number_token with value 0.0.
-		 */
-		Number_Token();
-
-		/**
-		 * @brief Creates a number_token with the specified value.
-		 */
-		Number_Token(double value);
-		~Number_Token() = default;
-
-		/**
-		 * @brief Accessor for the litteral value of a number_token;
-		 *
-		 * @return the value stored in the token.
-		 */
-		virtual double get_value() override;
-	};
-
-	/**
-	 * Holds the value of a variable's name, as well as a reference to all the 
-	 * variables to be able to look for its own value.
-	 */
-	class Name_Token : public Token {
-	private:
-		std::string name;
-		std::map<std::string, double>& variables;
-		Name_Token() = delete;
-	public:
-		/**
-		 * @brief The only constructor for a variable token.
-		 * 
-		 * @param name the name of the variable.
-		 * @param variables a reference to the table of all variables.
-		 */
-		Name_Token(std::string name, std::map<std::string, double>& variables);
-		~Name_Token() = default;
-
-		/**
-		 * @brief Looks for its own value in the table of variables and returns
-		 * it.
-		 *
-		 * @return the value of the variable of name `name` in the table.
-		 */
-		virtual double get_value() override;
-	};
-
-	/**
-	 * @brief Holds a binary operator, as well as its left and right sons.
-	 */
-	class Operator_Token : public Token {
-	private:
-		char op;
-		std::unique_ptr<Token> LHS, RHS;
-		Operator_Token() = delete;
-		Operator_Token(const Operator_Token&) = delete;
-		Operator_Token& operator=(const Operator_Token&) = delete;
-	public:
-		Operator_Token(
-			char op, 
-			std::unique_ptr<Operator_Token> LHS,
-			std::unique_ptr<Operator_Token> RHS
-		);
-		virtual double get_value() override;
-	};
-
 	/**
 	* @brief The parser class is responsible for reading the tokens of its lexer,
 	* ordering them in a tree structure (AST) and executing them in order. 
